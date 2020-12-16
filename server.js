b203fa3fe653fcfb2b9f50c58655cbea7e03f7a4
Alicia Gorton <apersigehl@gmail.com>
2020-12-16 12:18:05 -0800

    editing server.js to require cors, add routes and add connection to MongoDB

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
 const express = require("express");
+const cors = require("cors");
+
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+
 const mongoose = require("mongoose");
+const routes = require("./routes")
+
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Add routes, both API and view
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// Connect to the Mongo DB
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, 
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
-app.get("/books", (req, res) => {
-    db.Book.find({})
-      .then(dbBook => {
-        res.json(dbBook);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Established connection with MongoDB database.')
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
