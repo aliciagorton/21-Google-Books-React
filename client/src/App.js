bfdab5b6d8ba3f66a4358ed1ca70d7f848350943
Alicia Gorton <apersigehl@gmail.com>
2020-12-16 12:59:53 -0800

    outlined react app, created components needed, and restructered app.js to include react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar';
+import Saved from './components/Saved';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Saved />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
